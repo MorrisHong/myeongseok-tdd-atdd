56e9433ed0c892b07cd8452124e88a5eea5234cb
MorrisHong <govlmo91@gmail.com>
2021-04-23 04:59:26 +0900

    init

---
+++ comp/common-utils/src/main/java/com/sp/fc/Person.java
@@ -0,0 +1,17 @@
+package com.sp.fc;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * @author : Eunmo Hong
+ * @since : 2021/04/23
+ */
+
+
+@Builder
+@Getter
+public class Person {
+
+    private String name;
+}
---
+++ server/basic-test/src/main/java/com/sp/fc/BasicTestApplication.java
@@ -0,0 +1,17 @@
+package com.sp.fc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : Eunmo Hong
+ * @since : 2021/04/23
+ */
+
+@SpringBootApplication
+public class BasicTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicTestApplication.class, args);
+	}
+}
